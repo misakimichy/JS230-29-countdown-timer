d128ff7b79e188894f88c3fa919016819e4aeb07
Misaki Koonce <misaki.koonce@gmail.com>
2019-11-02 15:56:46 -0700

    Add displayEndTime function.

--- js/main.js
+++ js/main.js
@@ -4,9 +4,11 @@
     const endTime = document.querySelector('.display-end-time');
     const buttons = document.querySelectorAll('[data-time]');
 
-    const timer = (seconds, countdown) => {
+    const timer = (seconds) => {
         const now = Date.now();
+        const then = now + seconds * 1000;
         displayTimeLeft(seconds);
+        displayEndTime(then);
         countdown = setInterval(() => {
             const secondsLeft = Math.round((then - Date.now()) / 1000);
             if(secondsLeft < 0) {
@@ -17,13 +19,34 @@
         }, 1000);
     };
 
-    const displayTimeLeft = seconds => {
+    const displayTimeLeft = (seconds) => {
         const minutes = Math.floor(seconds / 60);
+        const remainderSeconds = seconds % 60;
+        const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+        if (timeDisplay) {
+            timeDisplay.textContent = display;
+        }
+        // Update the browser title
+        document.title = display;
+    };
+
+    const displayEndTime = (timestamp) => {
+        const end = new Date(timestamp);
+        const hour = end.getHours();
+        const minutes = end.getMinutes();
+
+        // 24h
+        endTime.textContent = `Be back at ${hour}:${minutes < 10 ? '0' : ''}`;
+        
+        // 12h
+        // endTime.textContent = `Be back at ${hour > 12 ? hour - 12 : hour}:${minutes}`;
     };
     
-    const startTimer = () => {
-        console.log('hello');
+    const startTimer = (e) => {
+        const seconds = parseInt(e.currentTarget.dataset.time);
+        timer(seconds);
     };
     
     buttons.forEach(button => button.addEventListener('click', startTimer));
+    timer(100);
 }());
